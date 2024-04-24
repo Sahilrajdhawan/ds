a01bf5f78348343163e1cd3a413db652218a9b39
Sahilrajdhawan <92576969+Sahilrajdhawan@users.noreply.github.com>
2024-04-24 10:30:05 +0530

    Update AddClient.java

--- assignment1ds/AddClient.java
+++ assignment1ds/AddClient.java
@@ -5,6 +5,7 @@ try {
 String addServerURL = "rmi://" + args[0] + "/AddServer";
 AddServerIntf addServerIntf =
 (AddServerIntf)Naming.lookup(addServerURL);
+//Here we didn't created the object we just stored it's refeence
 System.out.println("The first number is: " + args[1]);
 double d1 = Double.valueOf(args[1]).doubleValue();
 System.out.println("The second number is: " + args[2]);
